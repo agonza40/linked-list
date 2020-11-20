d10baf7fa49e3eeb4a57a645ae9771a0408407f9
Andy Gonzalez <agonzalez@glintinc.com>
2020-11-20 12:08:50 -0800

    More consistent spacing

--- spec/linked-list.spec.ts
+++ spec/linked-list.spec.ts
@@ -1,15 +1,18 @@
 import {LinkedList} from '../linked-list'
 
 describe('linked list', function () {
+
     describe('from', function () {
         it('constructs an empty list from empty argument list', function () {
             let list = LinkedList.from()
             expect(list.empty()).toBe(true)
         })
+
         it('constructs a list with 1 element', function () {
             let list = LinkedList.from('test')
             expect(list.length).toBe(1)
         })
+
         it('constructs a list from multiple elements', function () {
             let list = LinkedList.from('test', 'test2', 'test3', 'test4')
             expect(list.length).toBe(4)
@@ -67,12 +70,15 @@ describe('linked list', function () {
             const list = LinkedList.from(1, 2, 3, 4, 5)
             return list.at(x);
         }
+
         it('returns a the element at a given index', function () {
             expect(listAt(3)).toBe(4)
         })
+
         it('returns null for indices before the first', function () {
             expect(listAt(-1)).toBe(null)
         })
+
         it('returns null for indices after the last', function () {
             expect(listAt(10)).toBe(null)
         })
@@ -87,6 +93,7 @@ describe('linked list', function () {
             list.toArray().forEach((num) => expect(spy).toHaveBeenCalledWith(num))
         })
     })
+
     describe('map', function () {
         it('produces a new list with the results of a function called on each element', function () {
             const plusOne = (n:number) => n + 1,
@@ -95,18 +102,21 @@ describe('linked list', function () {
             expect(list.map(plusOne).toArray()).toEqual([2, 3, 4, 5, 6])
         })
     })
+
     describe('reduce', function () {
         it('uses a function to reduce the list elements to a single value', function () {
             const list: LinkedList<number> = LinkedList.from(1, 2, 3, 4, 5)
             expect(list.reduce((sum, n)=> sum + n, 0)).toBe(15)
         })
     })
+
     describe('cons', function () {
         it('provides a static method to join a new head with a list', function () {
             const list: LinkedList<number> = LinkedList.from(1, 2, 3, 4, 5)
             expect(LinkedList.cons(10, list).head).toBe(10)
         })
     })
+
     describe('reverse', function () {
         it('reverses the order of the elements in the list', function () {
             const list: LinkedList<number> = LinkedList.from(1, 2, 3, 4, 5)
@@ -127,12 +137,14 @@ describe('linked list', function () {
             expect(reversedList).toBe(list)
         })
     })
+
     describe('prepend', function () {
         it('adds to the front of the list', function () {
             const list = LinkedList.from(1)
             expect(list.prepend(0).toArray()).toEqual([0, 1])
         })
     })
+
     describe('append', function () {
         it('adds to the end of the list', function () {
             const list = LinkedList.from(1)
